505b0163042b5d332e7768b3f981caa5179ae533
AL4255 <92372109+AL4255@users.noreply.github.com>
2025-08-30 20:33:44 -0400

    Update sppython.py
    just paying around with the code for some packet info

--- sppython.py
+++ sppython.py
@@ -15,6 +15,17 @@ file_object = open('sample_logs.txt', 'r')
 
 new_da = file_object.readlines()
 
-print(len(new_da))
+#print(len(new_da))
+#print("First log entry:", new_da[0])
 
-print("First log entry:", new_da[0])
+for log_line in new_da:
+    if "logout" in log_line.lower():
+        logout_count += 1
+    if "login" in log_line.lower():
+        login_count += 1
+    if "[ERROR]" in log_line:
+        error_count += 1
+
+print("Number of logout events:", logout_count)
+print("Number of login events:", login_count)
+print("Number of error events:", error_count)
